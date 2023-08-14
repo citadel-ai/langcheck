b3ae69476bf9627c5d16674b899a89952a0711d8
Kenny Song <kenny.ysong@gmail.com>
2023-08-14 11:56:24 +0000

    Formatting tweak

--- tests/eval/test_text_structure.py
+++ tests/eval/test_text_structure.py
@@ -5,6 +5,10 @@ import pytest
 
 from langcheck.eval import is_float, is_int
 
+################################################################################
+# Utility functions
+################################################################################
+
 
 def is_close(a: List, b: List) -> bool:
     '''Returns True if two lists of numbers are element-wise close.'''
@@ -12,7 +16,12 @@ def is_close(a: List, b: List) -> bool:
     return all(math.isclose(x, y) for x, y in zip(a, b))
 
 
-@pytest.mark.parametrize("generated_outputs,domain,metric_values", [
+################################################################################
+# Tests
+################################################################################
+
+
+@pytest.mark.parametrize('generated_outputs,domain,metric_values', [
     (['-100', '-1', '0', '1', '100'], None, [1, 1, 1, 1, 1]),
     (['-100', '-1', '0', '1', '100'], range(-5, 6), [0, 1, 1, 1, 0]),
     (['-100', '-1', '0', '1', '100'], {0, 1, 2}, [0, 0, 1, 1, 0]),
@@ -27,7 +36,7 @@ def test_is_int(generated_outputs, domain, metric_values):
     assert is_close(eval_value.metric_values, metric_values)
 
 
-@pytest.mark.parametrize("generated_outputs,min,max,metric_values", [
+@pytest.mark.parametrize('generated_outputs,min,max,metric_values', [
     (['-100.5', '-1', '0', '1', '100.5'], None, None, [1, 1, 1, 1, 1]),
     (['-100.5', '-1', '0', '1', '100.5'], None, 5, [1, 1, 1, 1, 0]),
     (['-100.5', '-1', '0', '1', '100.5'], -5, None, [0, 1, 1, 1, 1]),
