a6b138ff5caf762f3adb523399afd2aad83a7fdb
Kenny Song <kenny.ysong@gmail.com>
2023-08-14 11:53:47 +0000

    Add the remaining text structure metrics with tests

--- src/langcheck/eval/__init__.py
+++ src/langcheck/eval/__init__.py
@@ -1,1 +1,5 @@
-from langcheck.eval.text_structure import is_float, is_int
+from langcheck.eval.text_structure import (contains_all_strings,
+                                           contains_any_strings,
+                                           contains_regex, is_float, is_int,
+                                           is_json_array, is_json_object,
+                                           matches_regex, run_valid_fn)
--- tests/eval/test_text_structure.py
+++ tests/eval/test_text_structure.py
@@ -1,9 +1,16 @@
+import json
 import math
 from typing import List
 
 import pytest
 
-from langcheck.eval import is_float, is_int
+from langcheck.eval import (contains_all_strings, contains_any_strings,
+                            contains_regex, is_float, is_int, is_json_array,
+                            is_json_object, matches_regex, run_valid_fn)
+
+################################################################################
+# Utility functions
+################################################################################
 
 
 def is_close(a: List, b: List) -> bool:
@@ -12,7 +19,12 @@ def is_close(a: List, b: List) -> bool:
     return all(math.isclose(x, y) for x, y in zip(a, b))
 
 
-@pytest.mark.parametrize("generated_outputs,domain,metric_values", [
+################################################################################
+# Tests
+################################################################################
+
+
+@pytest.mark.parametrize('generated_outputs,domain,metric_values', [
     (['-100', '-1', '0', '1', '100'], None, [1, 1, 1, 1, 1]),
     (['-100', '-1', '0', '1', '100'], range(-5, 6), [0, 1, 1, 1, 0]),
     (['-100', '-1', '0', '1', '100'], {0, 1, 2}, [0, 0, 1, 1, 0]),
@@ -27,7 +39,7 @@ def test_is_int(generated_outputs, domain, metric_values):
     assert is_close(eval_value.metric_values, metric_values)
 
 
-@pytest.mark.parametrize("generated_outputs,min,max,metric_values", [
+@pytest.mark.parametrize('generated_outputs,min,max,metric_values', [
     (['-100.5', '-1', '0', '1', '100.5'], None, None, [1, 1, 1, 1, 1]),
     (['-100.5', '-1', '0', '1', '100.5'], None, 5, [1, 1, 1, 1, 0]),
     (['-100.5', '-1', '0', '1', '100.5'], -5, None, [0, 1, 1, 1, 1]),
@@ -38,6 +50,133 @@ def test_is_int(generated_outputs, domain, metric_values):
 def test_is_float(generated_outputs, min, max, metric_values):
     eval_value = is_float(generated_outputs, min, max)
     assert eval_value.metric_name == 'is_float'
+    assert eval_value.prompts is None
+    assert eval_value.generated_outputs == generated_outputs
+    assert is_close(eval_value.metric_values, metric_values)
+
+
+@pytest.mark.parametrize('generated_outputs,metric_values', [
+    (['''[1, 2, 3]'''], [1]),
+    (['''["lorem", 1.5, -123.456, {"foo": 9999}, [1, 2, 3]]'''], [1]),
+    (['''[1, 2, 3'''], [0]),
+    (['''1, 2, 3]'''], [0]),
+    (['''1, 2, 3'''], [0]),
+    (['''[1, 2, 3]]'''], [0]),
+    (['''[[1, 2, 3]'''], [0]),
+    (['''"foo"'''], [0]),
+    (['''1'''], [0]),
+    (['''{"a": 1, "b": 2.5}'''], [0]),
+])
+def test_is_json_array(generated_outputs, metric_values):
+    eval_value = is_json_array(generated_outputs)
+    assert eval_value.metric_name == 'is_json_array'
+    assert eval_value.prompts is None
+    assert eval_value.generated_outputs == generated_outputs
+    assert is_close(eval_value.metric_values, metric_values)
+
+
+@pytest.mark.parametrize('generated_outputs,metric_values', [
+    (['''{"a": 1, "b": 2.5}'''], [1]),
+    (['''{"a": "foo", "b": -9999, "c": {"d": "e"}, "f": [1]}'''], [1]),
+    (['''"a": 1, "b": 2.5}'''], [0]),
+    (['''{"a": 1, "b": 2.5'''], [0]),
+    (['''"a": 1, "b": 2.5'''], [0]),
+    (['''{"a": 1, "b": 2.5}}'''], [0]),
+    (['''{{"a": 1, "b": 2.5}'''], [0]),
+    (['''"foo"'''], [0]),
+    (['''1'''], [0]),
+    (['''[1, 2, 3]'''], [0]),
+])
+def test_is_json_object(generated_outputs, metric_values):
+    eval_value = is_json_object(generated_outputs)
+    assert eval_value.metric_name == 'is_json_object'
+    assert eval_value.prompts is None
+    assert eval_value.generated_outputs == generated_outputs
+    assert is_close(eval_value.metric_values, metric_values)
+
+
+@pytest.mark.parametrize('generated_outputs,regex,metric_values', [
+    (['foo@example.com', 'his email is foo@example.com'
+      ], r'[^@^\s]+@[^@^\s]+\.[^@^\s]+', [1, 0]),
+    (['1234', '123456789', 'my ID is 123456789'], r'\d{5,}', [0, 1, 0]),
+    (['$123', '$123.45', '$123.4567', '¥123', 'the price is $123'
+      ], r'\$\d+(\.\d\d)?', [1, 1, 0, 0, 0]),
+])
+def test_matches_regex(generated_outputs, regex, metric_values):
+    eval_value = matches_regex(generated_outputs, regex)
+    assert eval_value.metric_name == 'matches_regex'
+    assert eval_value.prompts is None
+    assert eval_value.generated_outputs == generated_outputs
+    assert is_close(eval_value.metric_values, metric_values)
+
+
+@pytest.mark.parametrize('generated_outputs,regex,metric_values', [
+    (['foo@example.com', 'his email is foo@example.com'
+      ], r'[^@^\s]+@[^@^\s]+\.[^@^\s]+', [1, 1]),
+    (['1234', '123456789', 'my ID is 123456789'], r'\d{5,}', [0, 1, 1]),
+    (['$123', '$123.45', '$123.4567', '¥123', 'the price is $123'
+      ], r'\$\d+(\.\d\d)?', [1, 1, 1, 0, 1]),
+])
+def test_contains_regex(generated_outputs, regex, metric_values):
+    eval_value = contains_regex(generated_outputs, regex)
+    assert eval_value.metric_name == 'contains_regex'
+    assert eval_value.prompts is None
+    assert eval_value.generated_outputs == generated_outputs
+    assert is_close(eval_value.metric_values, metric_values)
+
+
+@pytest.mark.parametrize(
+    'generated_outputs,strings,case_sensitive,metric_values', [
+        (['As an AI language model, ...'], ['as an ai language model'
+                                            ], False, [1]),
+        (['As an AI language model, ...'], ['ai', 'language model'
+                                            ], False, [1]),
+        (['As an AI language model, ...'], ['ai', 'language model', 'foo'
+                                            ], False, [0]),
+        (['As an AI language model, ...'], ['as an ai language model'
+                                            ], True, [0]),
+    ])
+def test_contains_all_strings(generated_outputs, strings, case_sensitive,
+                              metric_values):
+    eval_value = contains_all_strings(generated_outputs, strings,
+                                      case_sensitive)
+    assert eval_value.metric_name == 'contains_all_strings'
+    assert eval_value.prompts is None
+    assert eval_value.generated_outputs == generated_outputs
+    assert is_close(eval_value.metric_values, metric_values)
+
+
+@pytest.mark.parametrize(
+    'generated_outputs,strings,case_sensitive,metric_values', [
+        (['As an AI language model, ...'], ['as an ai language model'
+                                            ], False, [1]),
+        (['As an AI language model, ...'], ['ai', 'language model'
+                                            ], False, [1]),
+        (['As an AI language model, ...'], ['ai', 'language model', 'foo'
+                                            ], False, [1]),
+        (['As an AI language model, ...'], ['as an ai language model'
+                                            ], True, [0]),
+    ])
+def test_contains_any_strings(generated_outputs, strings, case_sensitive,
+                              metric_values):
+    eval_value = contains_any_strings(generated_outputs, strings,
+                                      case_sensitive)
+    assert eval_value.metric_name == 'contains_any_strings'
+    assert eval_value.prompts is None
+    assert eval_value.generated_outputs == generated_outputs
+    assert is_close(eval_value.metric_values, metric_values)
+
+
+@pytest.mark.parametrize('generated_outputs,valid_fn,metric_values', [
+    (['2', '4', '9', '11'], lambda x: int(x) % 2 == 0, [1, 1, 0, 0]),
+    (['''{"myKey": 123}'''], lambda x: 'myKey' in json.loads(x), [1]),
+    (['''{"foo": 123}'''], lambda x: 'myKey' in json.loads(x), [0]),
+    (['''"myKey": 123'''], lambda x: 'myKey' in json.loads(x), [0]),
+    (['lorem ipsum'], lambda x: x / 0, [0]),
+])
+def test_run_valid_fn(generated_outputs, valid_fn, metric_values):
+    eval_value = run_valid_fn(generated_outputs, valid_fn)
+    assert eval_value.metric_name == 'run_valid_fn'
     assert eval_value.prompts is None
     assert eval_value.generated_outputs == generated_outputs
     assert is_close(eval_value.metric_values, metric_values)
