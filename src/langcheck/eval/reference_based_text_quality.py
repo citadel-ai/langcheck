c2029d9a73560d1018e9fbae9f94e78731efbe65
Yosuke Higashi <yosuke.higashi1@gmail.com>
2023-08-16 03:08:48 +0000

    check inputs + clip

--- src/langcheck/eval/reference_based_text_quality.py
+++ src/langcheck/eval/reference_based_text_quality.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import torch
 from sentence_transformers import SentenceTransformer, util
 
 from langcheck.eval.eval_value import EvalValue
@@ -22,11 +23,24 @@ def semantic_sim(generated_outputs: List[str],
     Returns:
         An EvalValue object
     '''
+    if len(generated_outputs) != len(reference_outputs):
+        raise ValueError(
+            'The generated and reference outputs lists must be of the same length'
+        )
+    if len(generated_outputs) == 0:
+        return EvalValue(metric_name='semantic_sim',
+                         prompts=None,
+                         generated_outputs=[],
+                         reference_outputs=[],
+                         metric_values=[])
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     generated_embeddings = model.encode(generated_outputs)
     reference_embeddings = model.encode(reference_outputs)
     cosine_scores = util.pairwise_cos_sim(generated_embeddings,
                                           reference_embeddings)
+    # Numerical instability can cause the dot product of almost identical
+    # vectors to exceed 1.0 slightly, so we clip the outputs
+    cosine_scores = torch.clamp(cosine_scores, -1.0, 1.0)
 
     return EvalValue(metric_name='semantic_sim',
                      prompts=None,
