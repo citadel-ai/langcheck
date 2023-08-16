f19e2aae747123618ced0af16d76f112925d069b
Yosuke Higashi <yosuke.higashi1@gmail.com>
2023-08-16 02:05:49 +0000

    add docstring

--- src/langcheck/eval/reference_based_text_quality.py
+++ src/langcheck/eval/reference_based_text_quality.py
@@ -3,8 +3,21 @@ from sentence_transformers import SentenceTransformer, util
 from langcheck.eval.eval_value import EvalValue
 
 
-# Compute semantic similarity
 def semantic_sim(generated_outputs, reference_outputs):
+    '''Calculates the semantic similarities between the generated outputs and
+    the reference outputs. The similarities are computed as the cosine
+    similarities between the generated and reference embeddings.
+
+    Ref:
+        https://huggingface.co/tasks/sentence-similarity
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+
+    Returns:
+        An EvalValue object
+    '''
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     generated_embeddings = [
         model.encode(sentence) for sentence in generated_outputs
