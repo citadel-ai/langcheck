2f82af49e643544e84a698cc07a111f68884614f
Koki Ryu <liukoki@gmail.com>
2023-08-18 06:59:37 +0000

    Add fluency metrics

--- src/langcheck/eval/__init__.py
+++ src/langcheck/eval/__init__.py
@@ -1,2 +1,3 @@
-from langcheck.eval.reference_free_text_quality import sentiment, toxicity
+from langcheck.eval.reference_free_text_quality import (fluency, sentiment,
+                                                        toxicity)
 from langcheck.eval.text_structure import is_float, is_int
--- src/langcheck/eval/reference_free_text_quality.py
+++ src/langcheck/eval/reference_free_text_quality.py
@@ -10,6 +10,10 @@ _sentiment_model_path = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 _sentiment_tokenizer = None
 _sentiment_model = None
 
+_fluency_model_path = "prithivida/parrot_fluency_model"
+_fluency_tokenizer = None
+_fluency_model = None
+
 _toxicity_model = None
 
 
@@ -49,6 +53,46 @@ def sentiment(generated_outputs: List[str]) -> EvalValue:
     scores = (probs[:, 1] / 2 + probs[:, 2]).tolist()
 
     return EvalValue(metric_name='sentiment',
+                     prompts=None,
+                     generated_outputs=generated_outputs,
+                     metric_values=scores)
+
+
+def fluency(generated_outputs: List[str]) -> EvalValue:
+    '''Calculates the fluency scores of the generated outputs between
+    0 (Negative) and 1 (Positive) based on the parrot_fluency_model.
+
+    Ref:
+    https://huggingface.co/prithivida/parrot_fluency_model
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+
+    Returns:
+        An EvalValue object
+    '''
+    global _fluency_tokenizer, _fluency_model
+
+    if _fluency_tokenizer is None or _fluency_model is None:
+        _fluency_tokenizer = AutoTokenizer.from_pretrained(_fluency_model_path)
+
+        # There is a "Some weights are not used warning" but we ignore it because
+        # that is intended.
+        _fluency_model = AutoModelForSequenceClassification.from_pretrained(
+            _fluency_model_path)
+
+    input_tokens = _fluency_tokenizer(generated_outputs,
+                                      return_tensors='pt',
+                                      padding=True)
+
+    with torch.no_grad():
+        # Probabilities of [negative, neutral, positive]
+        probs = torch.nn.functional.softmax(
+            _fluency_model(**input_tokens).logits, dim=1)
+
+    scores = probs[:, 1].tolist()
+
+    return EvalValue(metric_name='fluency',
                      prompts=None,
                      generated_outputs=generated_outputs,
                      metric_values=scores)
