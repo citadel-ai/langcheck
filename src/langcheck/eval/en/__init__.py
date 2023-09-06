d881e27f5010fadec20edef302c186d6ea8c5cb5
Yosuke Higashi <yosuke.higashi1@gmail.com>
2023-09-06 06:47:56 +0000

    add rouge scores

--- src/langcheck/eval/en/__init__.py
+++ src/langcheck/eval/en/__init__.py
@@ -1,3 +1,5 @@
-from langcheck.eval.en.reference_based_text_quality import semantic_sim
+from langcheck.eval.en.reference_based_text_quality import (semantic_sim,
+                                                            rouge1, rouge2,
+                                                            rougeL)
 from langcheck.eval.en.reference_free_text_quality import (
     flesch_kincaid_grade, flesch_reading_ease, fluency, sentiment, toxicity)
--- src/langcheck/eval/en/reference_based_text_quality.py
+++ src/langcheck/eval/en/reference_based_text_quality.py
@@ -5,6 +5,8 @@ from sentence_transformers import SentenceTransformer, util
 
 from langcheck.eval.eval_value import EvalValue
 
+from rouge_score import rouge_scorer
+
 
 def semantic_sim(generated_outputs: List[str],
                  reference_outputs: List[str]) -> EvalValue[float]:
@@ -54,3 +56,64 @@ def semantic_sim(generated_outputs: List[str],
                      reference_outputs=reference_outputs,
                      metric_values=cosine_scores.tolist(),
                      language='en')
+
+
+def rouge1(generated_outputs: List[str],
+           reference_outputs: List[str]) -> EvalValue[float]:
+    scores = _rouge(generated_outputs, reference_outputs, 'rouge1')
+    return EvalValue(metric_name='rouge1',
+                     prompts=None,
+                     generated_outputs=generated_outputs,
+                     reference_outputs=reference_outputs,
+                     metric_values=scores,
+                     language='en')
+
+
+def rouge2(generated_outputs: List[str],
+           reference_outputs: List[str]) -> EvalValue[float]:
+    scores = _rouge(generated_outputs, reference_outputs, 'rouge2')
+    return EvalValue(metric_name='rouge2',
+                     prompts=None,
+                     generated_outputs=generated_outputs,
+                     reference_outputs=reference_outputs,
+                     metric_values=scores,
+                     language='en')
+
+
+def rougeL(generated_outputs: List[str],
+           reference_outputs: List[str]) -> EvalValue[float]:
+    scores = _rouge(generated_outputs, reference_outputs, 'rougeLsum')
+    return EvalValue(metric_name='rougeL',
+                     prompts=None,
+                     generated_outputs=generated_outputs,
+                     reference_outputs=reference_outputs,
+                     metric_values=scores,
+                     language='en')
+
+
+def _rouge(generated_outputs: List[str], reference_outputs: List[str],
+           rouge_type: str) -> List[float]:
+    '''Calculates the semantic similarities between the generated outputs and
+    the reference outputs. The similarities are computed as the cosine
+    similarities between the generated and reference embeddings. This metric
+	takes on float values between [-1, 1], but typically ranges between 0 and 1
+    where 0 is minimum similariy and 1 is maximum similarity.
+
+    Ref:
+        https://huggingface.co/tasks/sentence-similarity
+        https://www.sbert.net/docs/usage/semantic_textual_similarity.html
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+
+    Returns:
+        An EvalValue object
+    '''
+    assert rouge_type in ["rouge1", "rouge2", "rougeLsum"]
+    scorer = rouge_scorer.RougeScorer([rouge_type], use_stemmer=True)
+    scores = []
+    for gen, ref in zip(generated_outputs, reference_outputs):
+        score = scorer.score(gen, ref)
+        scores.append(score[rouge_type].fmeasure)
+    return scores
