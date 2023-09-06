8357efabeaf148e325a0915366b12f945ac4d8a6
Yosuke Higashi <yosuke.higashi1@gmail.com>
2023-09-06 08:31:41 +0000

    docstrings

--- src/langcheck/eval/en/reference_based_text_quality.py
+++ src/langcheck/eval/en/reference_based_text_quality.py
@@ -60,6 +60,22 @@ def semantic_sim(generated_outputs: List[str],
 
 def rouge1(generated_outputs: List[str],
            reference_outputs: List[str]) -> EvalValue[float]:
+    '''Calculates the F1 metrics of the ROUGE-1 scores between the generated
+    outputs and the reference outputs. It evaluates the overlap of unigrams
+    (single tokens) between the generated outputs and the reference outputs.
+    This metric takes on float values between [0, 1], where 0 is no overlap and
+    1 is complete overlap.
+
+    Ref:
+        https://github.com/google-research/google-research/tree/master/rouge
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+
+    Returns:
+        An EvalValue object
+    '''
     scores = _rouge(generated_outputs, reference_outputs, 'rouge1')
     return EvalValue(metric_name='rouge1',
                      prompts=None,
@@ -71,6 +87,22 @@ def rouge1(generated_outputs: List[str],
 
 def rouge2(generated_outputs: List[str],
            reference_outputs: List[str]) -> EvalValue[float]:
+    '''Calculates the F1 metrics of the ROUGE-2 scores between the generated
+    outputs and the reference outputs. It evaluates the overlap of bigrams
+    (two adjacent tokens) between the generated outputs and the reference
+    outputs. This metric takes on float values between [0, 1], where 0 is no
+    overlap and 1 is complete overlap.
+
+    Ref:
+        https://github.com/google-research/google-research/tree/master/rouge
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+
+    Returns:
+        An EvalValue object
+    '''
     scores = _rouge(generated_outputs, reference_outputs, 'rouge2')
     return EvalValue(metric_name='rouge2',
                      prompts=None,
@@ -82,6 +114,32 @@ def rouge2(generated_outputs: List[str],
 
 def rougeL(generated_outputs: List[str],
            reference_outputs: List[str]) -> EvalValue[float]:
+    '''Calculates the F1 metrics of the ROUGE-L scores between the generated
+    outputs and the reference outputs. It evaluates the longest common
+    subsequence (LCS) between the generated outputs and the reference outputs.
+    This metric takes on float values between [0, 1], where 0 means that the LCS
+    is empty and 1 means that the reference and generated outputs are the same.
+
+    Ref:
+        https://github.com/google-research/google-research/tree/master/rouge
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+
+    Returns:
+        An EvalValue object
+    '''
+    # The `rouge_score` package has two flavors of ROUGE-L [1]:
+    # - 1) sentence-level, where newline characters are ignored
+    # - 2) summary-level, where newline characters are interpreted as sentence
+    #      boundaries
+    #
+    # We use (2) here (i.e. `rougeLsum`) because this is how `pyrouge` computes
+    # the ROUGE-L score (https://github.com/bheinzerling/pyrouge), which is a
+    # Python wrapper around original perl script implementation.
+    #
+    # [1] https://github.com/google-research/google-research/tree/master/rouge#two-flavors-of-rouge-l
     scores = _rouge(generated_outputs, reference_outputs, 'rougeLsum')
     return EvalValue(metric_name='rougeL',
                      prompts=None,
@@ -93,22 +151,17 @@ def rougeL(generated_outputs: List[str],
 
 def _rouge(generated_outputs: List[str], reference_outputs: List[str],
            rouge_type: str) -> List[float]:
-    '''Calculates the semantic similarities between the generated outputs and
-    the reference outputs. The similarities are computed as the cosine
-    similarities between the generated and reference embeddings. This metric
-	takes on float values between [-1, 1], but typically ranges between 0 and 1
-    where 0 is minimum similariy and 1 is maximum similarity.
-
-    Ref:
-        https://huggingface.co/tasks/sentence-similarity
-        https://www.sbert.net/docs/usage/semantic_textual_similarity.html
+    '''Helper function for computing the rouge1, rouge2, and rougeL metrics.
+    This uses Google Research's implementation of ROUGE:
+    https://github.com/google-research/google-research/tree/master/rouge
 
     Args:
         generated_outputs: A list of model generated outputs to evaluate
         reference_outputs: A list of reference outputs
+        rouge_type: rouge1, rouge2, or rougeLsum
 
     Returns:
-        An EvalValue object
+        A list of F1 values of the ROUGE scores
     '''
     assert rouge_type in ["rouge1", "rouge2", "rougeLsum"]
     scorer = rouge_scorer.RougeScorer([rouge_type], use_stemmer=True)
