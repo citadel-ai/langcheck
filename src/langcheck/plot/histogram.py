aca7f4765afadc84b8e783fce13780948b50b594
Kenny Song <kenny.ysong@gmail.com>
2023-09-11 12:22:34 +0000

    Change to slider

--- src/langcheck/plot/histogram.py
+++ src/langcheck/plot/histogram.py
@@ -1,5 +1,6 @@
 import plotly.express as px
 from dash import Dash, Input, Output, dcc, html
+from plotly.graph_objects import Figure, Histogram
 
 from langcheck.eval import EvalValue
 
@@ -16,9 +17,25 @@ def histogram(eval_value: EvalValue) -> None:
     app = Dash(__name__)
     app.layout = html.Div([
         html.Div([
-            html.Label('Number of bins: ', style={'background-color':
-                                                  'white'}),
-            dcc.Input(id='num_bins', type='number', value=10),
+            html.Label('Max number of bins: ',
+                       style={'background-color': 'white'}),
+            dcc.Slider(id='num_bins',
+                       min=1,
+                       max=50,
+                       step=1,
+                       value=10,
+                       marks={
+                           1: '1',
+                           10: '10',
+                           20: '20',
+                           30: '30',
+                           40: '40',
+                           50: '50'
+                       },
+                       tooltip={
+                           "placement": "bottom",
+                           "always_visible": True
+                       })
         ]),
         dcc.Graph(id='histogram',
                   config={
@@ -35,6 +52,8 @@ def histogram(eval_value: EvalValue) -> None:
     )
     def update_figure(num_bins):
         # Configure the actual histogram plot
+        # TODO: nbins is just a suggestion and not enforced. Figure out how to
+        # force it to use the exact number of bins the user selected.
         fig = px.histogram(df, nbins=int(num_bins), x=eval_value.metric_name)
 
         # If the user manually zoomed in, keep that zoom level even when
