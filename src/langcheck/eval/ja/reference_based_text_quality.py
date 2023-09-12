911d91f23b0f01b67f3192e984bc2d55ea5139a0
Koki Ryu <liukoki@gmail.com>
2023-09-12 08:59:54 +0000

    semantic sim function for japanese

---
+++ src/langcheck/eval/ja/reference_based_text_quality.py
@@ -0,0 +1,172 @@
+from typing import List
+
+import torch
+from sentence_transformers import SentenceTransformer, util
+
+from langcheck.eval.eval_value import EvalValue
+
+
+def semantic_sim(generated_outputs: List[str],
+                 reference_outputs: List[str]) -> EvalValue[float]:
+    '''Calculates the semantic similarities between the generated outputs and
+    the reference outputs. The similarities are computed as the cosine
+    similarities between the generated and reference embeddings. This metric
+	takes on float values between [-1, 1], but typically ranges between 0 and 1
+    where 0 is minimum similariy and 1 is maximum similarity.
+
+    Ref:
+        https://huggingface.co/tasks/sentence-similarity
+        https://www.sbert.net/docs/usage/semantic_textual_similarity.html
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+
+    Returns:
+        An EvalValue object
+    '''
+    if len(generated_outputs) != len(reference_outputs):
+        raise ValueError(
+            'The generated and reference outputs lists must be of the same length'
+        )
+    if len(generated_outputs) == 0:
+        return EvalValue(metric_name='semantic_sim',
+                         prompts=None,
+                         generated_outputs=[],
+                         reference_outputs=[],
+                         metric_values=[],
+                         language='en')
+    # According to the blog post,
+    # 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2' has the best
+    # performance on Japanese dataset.
+    # Ref: https://tech.yellowback.net/posts/sentence-transformers-japanese-models
+    model = SentenceTransformer(
+        'sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+    generated_embeddings = model.encode(generated_outputs)
+    reference_embeddings = model.encode(reference_outputs)
+    cosine_scores = util.pairwise_cos_sim(generated_embeddings,
+                                          reference_embeddings)
+    # Numerical instability can cause the dot product of almost identical
+    # vectors to exceed 1.0 slightly, so we clip the outputs
+    cosine_scores = torch.clamp(cosine_scores, -1.0, 1.0)
+
+    return EvalValue(metric_name='semantic_sim',
+                     prompts=None,
+                     generated_outputs=generated_outputs,
+                     reference_outputs=reference_outputs,
+                     metric_values=cosine_scores.tolist(),
+                     language='ja')
+
+
+def rouge1(generated_outputs: List[str],
+           reference_outputs: List[str]) -> EvalValue[float]:
+    '''Calculates the F1 metrics of the ROUGE-1 scores between the generated
+    outputs and the reference outputs. It evaluates the overlap of unigrams
+    (single tokens) between the generated outputs and the reference outputs.
+    This metric takes on float values between [0, 1], where 0 is no overlap and
+    1 is complete overlap.
+
+    Ref:
+        https://github.com/google-research/google-research/tree/master/rouge
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+
+    Returns:
+        An EvalValue object
+    '''
+    scores = _rouge(generated_outputs, reference_outputs, 'rouge1')
+    return EvalValue(metric_name='rouge1',
+                     prompts=None,
+                     generated_outputs=generated_outputs,
+                     reference_outputs=reference_outputs,
+                     metric_values=scores,
+                     language='en')
+
+
+def rouge2(generated_outputs: List[str],
+           reference_outputs: List[str]) -> EvalValue[float]:
+    '''Calculates the F1 metrics of the ROUGE-2 scores between the generated
+    outputs and the reference outputs. It evaluates the overlap of bigrams
+    (two adjacent tokens) between the generated outputs and the reference
+    outputs. This metric takes on float values between [0, 1], where 0 is no
+    overlap and 1 is complete overlap.
+
+    Ref:
+        https://github.com/google-research/google-research/tree/master/rouge
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+
+    Returns:
+        An EvalValue object
+    '''
+    scores = _rouge(generated_outputs, reference_outputs, 'rouge2')
+    return EvalValue(metric_name='rouge2',
+                     prompts=None,
+                     generated_outputs=generated_outputs,
+                     reference_outputs=reference_outputs,
+                     metric_values=scores,
+                     language='en')
+
+
+def rougeL(generated_outputs: List[str],
+           reference_outputs: List[str]) -> EvalValue[float]:
+    '''Calculates the F1 metrics of the ROUGE-L scores between the generated
+    outputs and the reference outputs. It evaluates the longest common
+    subsequence (LCS) between the generated outputs and the reference outputs.
+    This metric takes on float values between [0, 1], where 0 means that the LCS
+    is empty and 1 means that the reference and generated outputs are the same.
+
+    Ref:
+        https://github.com/google-research/google-research/tree/master/rouge
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+
+    Returns:
+        An EvalValue object
+    '''
+    # The `rouge_score` package has two flavors of ROUGE-L [1]:
+    # - 1) sentence-level, where newline characters are ignored
+    # - 2) summary-level, where newline characters are interpreted as sentence
+    #      boundaries
+    #
+    # We use (2) here (i.e. `rougeLsum`) because this is how `pyrouge` computes
+    # the ROUGE-L score (https://github.com/bheinzerling/pyrouge), which is a
+    # Python wrapper around original perl script implementation.
+    #
+    # [1] https://github.com/google-research/google-research/tree/master/rouge#two-flavors-of-rouge-l
+    scores = _rouge(generated_outputs, reference_outputs, 'rougeLsum')
+    return EvalValue(metric_name='rougeL',
+                     prompts=None,
+                     generated_outputs=generated_outputs,
+                     reference_outputs=reference_outputs,
+                     metric_values=scores,
+                     language='en')
+
+
+def _rouge(generated_outputs: List[str], reference_outputs: List[str],
+           rouge_type: str) -> List[float]:
+    '''Helper function for computing the rouge1, rouge2, and rougeL metrics.
+    This uses Google Research's implementation of ROUGE:
+    https://github.com/google-research/google-research/tree/master/rouge
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+        reference_outputs: A list of reference outputs
+        rouge_type: rouge1, rouge2, or rougeLsum
+
+    Returns:
+        A list of F1 values of the ROUGE scores
+    '''
+    assert rouge_type in ["rouge1", "rouge2", "rougeLsum"]
+    scorer = rouge_scorer.RougeScorer([rouge_type], use_stemmer=True)
+    scores = []
+    for gen, ref in zip(generated_outputs, reference_outputs):
+        score = scorer.score(gen, ref)
+        scores.append(score[rouge_type].fmeasure)
+    return scores
