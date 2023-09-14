c040bcea93896c9a0ee34ac1732c652c2c5ace02
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-09-14 06:04:52 +0000

    Implement rouge for Japanese

---
+++ tests/eval/ja/test_reference_based_text_quality.py
@@ -0,0 +1,75 @@
+import pytest
+
+from langcheck.eval.ja import rouge1, rouge2, rougeL
+from tests.utils import is_close
+from langcheck.eval.eval_value import EvalValue
+from langcheck.eval.ja._tokenizers import _JapaneseTokenizer
+from langcheck.eval.ja import JanomeTokenizer, MecabTokeninzer
+from typing import Optional, Callable
+
+################################################################################
+# Tests
+################################################################################
+
+parametrize_rouge_function = pytest.mark.parametrize("rouge_function",
+                                                     [rouge1, rouge2, rougeL])
+parametrize_tokenizer = pytest.mark.parametrize(
+    'tokenizer', [None, JanomeTokenizer, MecabTokeninzer])
+
+
+@pytest.mark.parametrize('generated_outputs,reference_outputs',
+                         [(["猫がマットの上に座った。"], ["猫がマットの上に座った。"])])
+@parametrize_rouge_function
+@parametrize_tokenizer
+def test_rouge_identical(generated_outputs: str, reference_outputs: str,
+                         rouge_function: Callable[[str, str],
+                                                  EvalValue[float]],
+                         tokenizer: Optional[_JapaneseTokenizer]) -> None:
+    # All ROUGE scores are 1 if the generated and reference outputs are
+    # identical
+    actual_eval_value = rouge_function(
+        generated_outputs,
+        reference_outputs,
+        tokenizer=tokenizer() if tokenizer else None)
+    assert actual_eval_value.metric_values == [1.]
+    assert actual_eval_value.language == 'ja'
+
+
+@pytest.mark.parametrize('generated_outputs,reference_outputs',
+                         [(["猫が、マットの上に座った。"], ["私は、アイスクリームを食べます。"])])
+@parametrize_rouge_function
+@parametrize_tokenizer
+def test_rouge_no_overlap(generated_outputs: str, reference_outputs: str,
+                          rouge_function: Callable[[str, str],
+                                                   EvalValue[float]],
+                          tokenizer: Optional[_JapaneseTokenizer]) -> None:
+    # All ROUGE scores are 0 if the generated and reference outputs have no
+    # overlapping words
+    actual_eval_value = rouge_function(
+        generated_outputs,
+        reference_outputs,
+        tokenizer=tokenizer() if tokenizer else None)
+    assert actual_eval_value.metric_values == [0.]
+    assert actual_eval_value.language == 'ja'
+
+
+@pytest.mark.parametrize('generated_outputs,reference_outputs',
+                         [(["猫がマットの上に座った。"], ["猫がマットの上に座っている。"])])
+@parametrize_rouge_function
+@parametrize_tokenizer
+def test_rouge_some_overlap(generated_outputs: str, reference_outputs: str,
+                            rouge_function: Callable[[str, str],
+                                                     EvalValue[float]],
+                            tokenizer: Optional[_JapaneseTokenizer]) -> None:
+    expected_value = {
+        rouge1: [0.823529411764706],
+        rouge2: [0.7999999999999999],
+        rougeL: [0.823529411764706]
+    }
+    # The ROUGE-2 score is lower than the ROUGE-1 and ROUGE-L scores
+    actual_eval_value = rouge_function(
+        generated_outputs,
+        reference_outputs,
+        tokenizer=tokenizer() if tokenizer else None)
+    is_close(actual_eval_value.metric_values, expected_value[rouge_function])
+    assert actual_eval_value.language == 'ja'
