b0b5c1ac742c0dd5a5618386db6884ebb7cd3141
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-09-14 06:04:01 +0000

    Publish tokenizers

--- src/langcheck/eval/ja/__init__.py
+++ src/langcheck/eval/ja/__init__.py
@@ -1,1 +1,4 @@
+from langcheck.eval.ja.reference_based_text_quality import (rouge1, rouge2,
+                                                            rougeL)
 from langcheck.eval.ja.reference_free_text_quality import sentiment
+from langcheck.eval.ja._tokenizers import JanomeTokenizer, MecabTokeninzer
