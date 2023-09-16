dcaaf209c81162a17f37a3473a3c04b001aa4dad
Kenny Song <kenny.ysong@gmail.com>
2023-09-16 06:08:20 +0000

    Try fixing the load_json test

--- tests/utils/test_io.py
+++ tests/utils/test_io.py
@@ -22,6 +22,7 @@ def test_load_json(json_data):
     # Save to JSON file in /tmp
     with tempfile.NamedTemporaryFile(mode='w', suffix='.json') as f:
         json.dump(json_data, f)
+        f.flush()  # Ensure the data is written to the file
         loaded_json_data = load_json(f.name)
 
     assert loaded_json_data == json_data
