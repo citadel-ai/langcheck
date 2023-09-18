5a8656a3bb6dc2a99f0f1d321bbe7ec9ed9c9195
Kenny Song <kenny.ysong@gmail.com>
2023-09-18 12:42:08 +0000

    add myst_parser

--- docs/conf.py
+++ docs/conf.py
@@ -23,11 +23,13 @@ author = 'Citadel AI'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode'
+    'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode',
+    'myst_parser'
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
