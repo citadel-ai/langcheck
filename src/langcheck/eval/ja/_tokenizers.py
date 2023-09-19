0862a6b8c3b9aed7e702431748199e226f92b389
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-09-19 04:18:06 +0000

    Add note to descrive Janome vs MeCab

--- src/langcheck/eval/ja/_tokenizers.py
+++ src/langcheck/eval/ja/_tokenizers.py
@@ -25,6 +25,18 @@ class _JapaneseTokenizer(BaseTokenizer):
 
 
 class MeCabTokenizer(_JapaneseTokenizer):
+    '''
+    An optional tokenizer for caliculating rouge score base on MeCab.
+
+    .. note::
+        The advantage of using MeCab is that the core implementation is written
+        in a compiled language and runs much faster than Janome. If you are
+        processing large data, consider setting up MeCab and using the
+        MeCabTokenizer.
+        On the other hand, it takes more effort to install it on some
+        environments and may not work. Please refer to the official page if the
+        Python wrapper, mecab-python3, does not work in your environment.
+    '''
 
     class _MeCabNodeSurfaceIterator(Iterator):
 
@@ -51,8 +63,17 @@ class MeCabTokenizer(_JapaneseTokenizer):
 class JanomeTokenizer(_JapaneseTokenizer):
     '''Janome based Tokenizer for Japanese.
 
-    The default tokenizer for caliculating rouge score.
-    Janome is a pure python library and introduces no additional dependency.
+    The default tokenizer for caliculating rouge score base on Janome.
+
+    .. note::
+        The advantage of using Janome is that it is a pure Python library and
+        introduces no additional dependencies.
+        On the other hand, it takes more time to parse sentences than a MeCab
+        -based tokenizer. Specifically, it takes seconds every time when
+        constructing this class since the Janome tokenizer loads the entire
+        dictionary during initialization.
+        If you are processing large data, consider setting up MeCab and using
+        the MeCabTokenizer.
     '''
 
     def __init__(self):
