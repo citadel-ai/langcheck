08faba44571e7c9980946360eb9d86b216d404b4
Kenny Song <kenny.ysong@gmail.com>
2023-09-19 09:35:14 +0000

    Change furo theme to sphinx-book-theme with custom.css

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ source_suffix = ['.rst', '.md']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 html_title = 'LangCheck ✅'
