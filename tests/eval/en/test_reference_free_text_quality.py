c526fc10f5820a7fb02aec1ef23079b98afdda99
Yosuke Higashi <yosuke.higashi1@gmail.com>
2023-09-27 04:03:18 +0000

    add test for sentiment openai

--- tests/eval/en/test_reference_free_text_quality.py
+++ tests/eval/en/test_reference_free_text_quality.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock, patch
+
 import pytest
 
 from langcheck.eval.en import (flesch_kincaid_grade, flesch_reading_ease,
@@ -15,6 +17,26 @@ from tests.utils import is_close
 def test_sentiment(generated_outputs):
     eval_value = sentiment(generated_outputs)
     assert all(0 <= v <= 1 for v in eval_value.metric_values)
+
+
+@pytest.mark.parametrize('generated_outputs', ["I'm fine!"])
+def test_sentiment_openai(generated_outputs):
+    mock_chat_response = {
+        'choices': [{
+            'message': {
+                'function_call': {
+                    'arguments': "{\n  \"sentiment\": \"Positive\"\n}"
+                }
+            }
+        }]
+    }
+    # Calling the openai.ChatCompletion.create method requires an OpenAI API
+    # key, so we mock the return value instead
+    with patch('openai.ChatCompletion.create',
+               Mock(return_value=mock_chat_response)):
+        eval_value = sentiment(generated_outputs, model_type='openai')
+        # "Positive" gets a value of 1.0
+        assert eval_value.metric_values[0] == 1
 
 
 @pytest.mark.parametrize(
