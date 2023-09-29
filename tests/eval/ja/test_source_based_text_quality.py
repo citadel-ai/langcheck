d2eeac69b297a9b26b28802b6296c0190d8fe73b
Koki Ryu <liukoki@gmail.com>
2023-09-29 07:05:38 +0000

    Add unit tests for ja openai metrics

--- tests/eval/ja/test_source_based_text_quality.py
+++ tests/eval/ja/test_source_based_text_quality.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock, patch
+
 import pytest
 
 from langcheck.eval.ja import factual_consistency
@@ -16,3 +18,26 @@ def test_factual_consistency(generated_outputs, sources):
     assert 0.9 <= factual_consistency_high <= 1
     factual_consistency_low = eval_value.metric_values[1]
     assert 0.0 <= factual_consistency_low <= 0.1
+
+
+@pytest.mark.parametrize('generated_outputs,sources',
+                         [(['東京は日本の首都です。'], ["東京は日本の首都です。"])])
+def test_factual_consistency_openai(generated_outputs, sources):
+    mock_chat_response = {
+        'choices': [{
+            'message': {
+                'function_call': {
+                    'arguments': "{\n  \"factuality\": \"Fully Consistent\"\n}"
+                }
+            }
+        }]
+    }
+    # Calling the openai.ChatCompletion.create method requires an OpenAI API
+    # key, so we mock the return value instead
+    with patch('openai.ChatCompletion.create',
+               Mock(return_value=mock_chat_response)):
+        eval_value = factual_consistency(generated_outputs,
+                                         sources,
+                                         model_type='openai')
+        # "Fully Consistent" gets a value of 1.0
+        assert eval_value.metric_values[0] == 1
