f9e67e7da84823aeeffc5398229e8f9d5aca4f6a
Koki Ryu <liukoki@gmail.com>
2023-10-04 12:45:21 +0000

    Implement japanese reading ease

--- src/langcheck/eval/ja/__init__.py
+++ src/langcheck/eval/ja/__init__.py
@@ -2,16 +2,12 @@ from langcheck.eval.ja._tokenizers import JanomeTokenizer, MeCabTokenizer
 from langcheck.eval.ja.reference_based_text_quality import (rouge1, rouge2,
                                                             rougeL,
                                                             semantic_sim)
-from langcheck.eval.ja.reference_free_text_quality import sentiment
+from langcheck.eval.ja.reference_free_text_quality import (
+    sentiment, tateishi_ono_yamada_reading_ease)
 from langcheck.eval.ja.source_based_text_quality import factual_consistency
 
 __all__ = [
-    'factual_consistency',
-    'JanomeTokenizer',
-    'MeCabTokenizer',
-    'rouge1',
-    'rouge2',
-    'rougeL',
-    'semantic_sim',
-    'sentiment',
+    'factual_consistency', 'JanomeTokenizer', 'MeCabTokenizer', 'rouge1',
+    'rouge2', 'rougeL', 'semantic_sim', 'sentiment',
+    'tateishi_ono_yamada_reading_ease'
 ]
