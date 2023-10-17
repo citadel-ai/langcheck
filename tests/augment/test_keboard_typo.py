b7c3c80ad959d44fd97fb439808b3e19d539cd15
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-10-17 16:53:02 +0900

    Add test for keyboard_typo

---
+++ tests/augment/test_keboard_typo.py
@@ -0,0 +1,20 @@
+import random
+from typing import List
+
+import pytest
+
+from langcheck.augment import keyboard_typo
+
+
+@pytest.mark.parametrize(
+    "texts, expected",
+    [
+        (["Hello, world!"], ["HF;lo, eorlE!"]),
+        (["Hello, world!", "I'm hungry"], ["HF;lo, eorlE!", "I ' m Y Tngry"]),
+    ],
+)
+def test_keyboard_typo(texts: List[str], expected: List[str]):
+    seed = 42
+    random.seed(seed)
+    actual = keyboard_typo(texts)
+    assert actual == expected
