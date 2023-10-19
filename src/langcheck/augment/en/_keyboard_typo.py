812e13138d5546c41d018091c8cc6bba72b629e6
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-10-19 23:19:36 +0900

    Add docstrings

--- src/langcheck/augment/en/_keyboard_typo.py
+++ src/langcheck/augment/en/_keyboard_typo.py
@@ -7,5 +7,16 @@ def keyboard_typo(
     texts: List[str],
     **kwargs,
 ) -> List[str]:
+    '''Generate keyboard typo perturbed texts for augmentation.
+
+    Args:
+        texts: List of texts to be augmented.
+        **kwargs: Arbitrary keyword arguments, which will be passed to
+            :class:`nlpaug.augmenter.char.keyboard.KeyboardAug`. See the
+            documentation for more details.
+
+    Returns:
+        A list of perturbed texts.
+    '''
     aug = nac.KeyboardAug()
     return aug.augment(texts, **kwargs)
