5775394639dfdc6d92a16c54fba8b5a66d412000
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-10-24 15:58:24 +0900

    Implement synonym

--- src/langcheck/augment/en/__init__.py
+++ src/langcheck/augment/en/__init__.py
@@ -1,5 +1,4 @@
 from langcheck.augment.en._keyboard_typo import keyboard_typo
+from langcheck.augment.en._synonym import synonym
 
-__all__ = [
-    'keyboard_typo',
-]
+__all__ = ['keyboard_typo', 'synonym']
---
+++ src/langcheck/augment/en/_synonym.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from nlpaug.augmenter.word import SynonymAug
+
+
+def synonym(
+    texts: list[str] | str,
+    **kwargs,
+) -> list[str]:
+    '''Generate texts where some of the input words are replaced with synonyms.
+
+    Args:
+        texts: List of texts to be augmented.
+        aug_p: Percentage of words which will be augmented. Default to `0.1`.
+        aug_max: Maximum number of words which will be augmented. Default to
+            `None`.
+
+    .. note::
+        Any argument that can be passed to
+        `nlpaug.augmenter.word.SynonymAug
+        <https://nlpaug.readthedocs.io/en/latest/_modules/nlpaug/augmenter/word/synonym.html>`_
+        is acceptable. Some of the more useful ones from `nlpaug` document are
+        listed below:
+            - ``aug_p`` (float): Percentage of word will be augmented.
+            - ``aug_min``: Minimum number of word will be augmented.
+            - ``aug_max``: Maximum number of word will be augmented.
+        Note that the default values for these arguments are different from the
+        ``nlpaug`` defaults. To be more specific, the default values for
+        ``aug_p`` to be `0.1` and ``aug_char_max`` to be `None`. See the source
+        code for mode details.
+
+    Returns:
+        A list of perturbed texts.
+    '''
+
+    kwargs["aug_p"] = kwargs.get("aug_p", 0.1)
+    kwargs["aug_max"] = kwargs.get("aug_max")
+
+    aug = SynonymAug(**kwargs)
+    return aug.augment(texts)
