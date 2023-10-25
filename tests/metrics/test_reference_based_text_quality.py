943b9698a9ad1b15d948c0883a888d3f1cc69f01
Yosuke Higashi <yosuke.higashi1@gmail.com>
2023-10-25 03:01:38 +0000

    update exact match test

--- tests/metrics/test_reference_based_text_quality.py
+++ tests/metrics/test_reference_based_text_quality.py
@@ -13,8 +13,7 @@ from langcheck.metrics import exact_match
      (["The cat sat on the mat."], ["The cat sat on the mat."])])
 def test_exact_match(generated_outputs, reference_outputs):
     metric_value = exact_match(generated_outputs, reference_outputs)
-    exact_match_value = metric_value.metric_values[0]
-    assert exact_match_value == 1
+    assert metric_value == 1
 
 
 @pytest.mark.parametrize(
@@ -23,5 +22,4 @@ def test_exact_match(generated_outputs, reference_outputs):
      (["The CAT sat on the MAT."], ["The cat sat on the mat."])])
 def test_not_exact_match(generated_outputs, reference_outputs):
     metric_value = exact_match(generated_outputs, reference_outputs)
-    exact_match_value = metric_value.metric_values[0]
-    assert exact_match_value == 0
+    assert metric_value == 0
