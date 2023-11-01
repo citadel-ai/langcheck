cf55d03fd64047826b44d870a5d20d91272fd3e8
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-11-01 11:57:04 +0900

    Activate import path langcheck.augment.ocr_typo

--- src/langcheck/augment/__init__.py
+++ src/langcheck/augment/__init__.py
@@ -1,4 +1,5 @@
 from langcheck.augment import en
 from langcheck.augment.en import keyboard_typo
+from langcheck.augment.en import ocr_typo
 
-__all__ = ["en", "keyboard_typo"]
+__all__ = ["en", "keyboard_typo", "ocr_typo"]
