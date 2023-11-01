7fbdc613dc5c8f7b0c2dbb8f9dfa327df32489c7
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-11-01 11:54:39 +0900

    Activate import path langcheck.augment.synonym

--- src/langcheck/augment/__init__.py
+++ src/langcheck/augment/__init__.py
@@ -1,4 +1,5 @@
 from langcheck.augment import en
 from langcheck.augment.en import keyboard_typo
+from langcheck.augment.en import synonym
 
-__all__ = ["en", "keyboard_typo"]
+__all__ = ["en", "keyboard_typo", "synonym"]
