7beb38a6539ff6eb7d767a391320891ed4c8624e
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-11-01 12:01:07 +0900

    Apply isort

--- src/langcheck/augment/__init__.py
+++ src/langcheck/augment/__init__.py
@@ -1,5 +1,4 @@
 from langcheck.augment import en
-from langcheck.augment.en import keyboard_typo
-from langcheck.augment.en import ocr_typo
+from langcheck.augment.en import keyboard_typo, ocr_typo
 
 __all__ = ["en", "keyboard_typo", "ocr_typo"]
