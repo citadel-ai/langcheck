c10d877e330ba5eb118e94f9c3a1c370c358d69b
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-11-08 14:39:09 +0900

    Implement augment.gender

--- src/langcheck/augment/en/__init__.py
+++ src/langcheck/augment/en/__init__.py
@@ -1,8 +1,10 @@
+from langcheck.augment.en._gender._gender import gender
 from langcheck.augment.en._keyboard_typo import keyboard_typo
 from langcheck.augment.en._ocr_typo import ocr_typo
 from langcheck.augment.en._synonym import synonym
 
 __all__ = [
+    'gender',
     'keyboard_typo',
     'ocr_typo',
     'synonym',
---
+++ tests/augment/test_gender.py
@@ -0,0 +1,49 @@
+import random
+from typing import List, Optional
+
+import pytest
+
+from langcheck.augment.en import gender
+
+
+def test_invalid_to_gender():
+    with pytest.raises(ValueError):
+        gender("text", to_gender="invalid option")
+
+
+def test_invalid_input():
+    with pytest.raises(TypeError):
+        gender(1)
+
+
+@pytest.mark.parametrize(
+    "texts",
+    [
+        (["He cooks by himself.", "This is his dog.", "I gave him a book."]),
+        (["She cooks by herself.", "This is her dog.", "I gave her a book."]),
+        ([
+            "They cooks by themselves.", "This is their dog.",
+            "I gave them a book."
+        ]),
+    ],
+)
+@pytest.mark.parametrize("to_gender, expected", [
+    (None, [
+        "They cooks by themselves.", "This is their dog.", "I gave them a book."
+    ]),
+    ("female",
+     ["She cooks by herself.", "This is her dog.", "I gave her a book."]),
+    ("male", ["He cooks by himself.", "This is his dog.", "I gave him a book."
+             ]),
+    ("neutral",
+     ['Xe cooks by xyrself.', 'This is xyr dog.', 'I gave xem a book.']),
+])
+def test_gender(
+    texts: List[str],
+    to_gender: Optional[str],
+    expected: List[str],
+):
+    seed = 42
+    random.seed(seed)
+    actual = gender(texts, to_gender=to_gender)
+    assert actual == expected
