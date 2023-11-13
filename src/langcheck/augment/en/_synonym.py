cf9080b8f1f2c0f431b4dad90b6ad3bf0cd38f83
Kenny Song <kenny.ysong@gmail.com>
2023-11-13 02:06:01 +0000

    Add num_perturbations param

--- src/langcheck/augment/en/_synonym.py
+++ src/langcheck/augment/en/_synonym.py
@@ -4,39 +4,46 @@ from nlpaug.augmenter.word import SynonymAug
 
 
 def synonym(
-    texts: list[str] | str,
+    instances: list[str] | str,
+    num_perturbations: int = 1,
     **kwargs,
 ) -> list[str]:
-    '''Generate texts where some of the input words are replaced with synonyms.
+    '''Applies a text perturbation to each string in instances (usually a list
+    of prompts) where some words are replaced with synonyms.
 
     Args:
-        texts: List of texts to be augmented.
-        aug_p: Percentage of words which will be augmented. Default to `0.1`.
-        aug_max: Maximum number of words which will be augmented. Default to
+        instances: A single string or a list of strings to be augmented.
+        num_perturbations: The number of perturbed instances to generate for
+            each string in instances
+        aug_p: Percentage of words which will be augmented. Defaults to `0.1`.
+        aug_max: Maximum number of words which will be augmented. Defaults to
             `None`.
 
     .. note::
         Any argument that can be passed to
         `nlpaug.augmenter.word.SynonymAug
         <https://nlpaug.readthedocs.io/en/latest/_modules/nlpaug/augmenter/word/synonym.html>`_
-        is acceptable. Some of the more useful ones from `nlpaug` document are
-        listed below:
+        is acceptable. Some of the more useful ones from the `nlpaug`
+        documention are listed below:
 
-          - ``aug_p`` (float): Percentage of word will be augmented.
-          - ``aug_min``: Minimum number of word will be augmented.
-          - ``aug_max``: Maximum number of word will be augmented.
+          - ``aug_p`` (float): Percentage of words which will be augmented.
+          - ``aug_min`` (int): Minimum number of words that will be augmented.
+          - ``aug_max`` (int): Maximum number of words that will be augmented.
 
-        Note that the default values for these arguments are different from the
-        ``nlpaug`` defaults. To be more specific, the default values for
-        ``aug_p`` to be `0.1` and ``aug_max`` to be `None`. See the source
-        code for mode details.
+        Note that the default values for these arguments may be different from
+        the ``nlpaug`` defaults.
 
     Returns:
-        A list of perturbed texts.
+        A list of perturbed instances.
     '''
 
     kwargs["aug_p"] = kwargs.get("aug_p", 0.1)
     kwargs["aug_max"] = kwargs.get("aug_max")
 
+    instances = [instances] if isinstance(instances, str) else instances
+    perturbed_instances = []
     aug = SynonymAug(**kwargs)
-    return aug.augment(texts)
+    for instance in instances:
+        for _ in range(num_perturbations):
+            perturbed_instances += aug.augment(instance)
+    return perturbed_instances
--- tests/augment/test_synonym.py
+++ tests/augment/test_synonym.py
@@ -8,15 +8,23 @@ from langcheck.augment.en import synonym
 
 
 @pytest.mark.parametrize(
-    "texts, expected",
+    "instances, num_perturbations, expected",
     [
-        (["Hello, world!"], ["Hullo, world!"]),
+        ("Hello, world!", 1, ["Hullo, world!"]),
+        ("Hello, world!", 2, ["Hullo, world!", "Hello, earth!"]),
+        (["Hello, world!"], 1, ["Hullo, world!"]),
+        ("Hello, world!", 2, ["Hullo, world!", "Hello, earth!"]),
         (["Hello, world!", "I have a pen. I have an apple"
-         ], ["Hullo, world!", "I have a pen. I have an malus pumila"]),
+         ], 1, ["Hullo, world!", "I have a pen. I have an malus pumila"]),
+        (["Hello, world!", "I have a pen. I have an apple"], 2, [
+            "Hullo, world!", "Hello, earth!", "I get a pen. I have an apple",
+            "I have a pen. I have an orchard apple tree"
+        ]),
     ],
 )
-def test_synonym(texts: list[str], expected: list[str]):
+def test_synonym(instances: list[str] | str, num_perturbations: int,
+                 expected: list[str]):
     seed = 42
     random.seed(seed)
-    actual = synonym(texts)
+    actual = synonym(instances, num_perturbations)
     assert actual == expected
