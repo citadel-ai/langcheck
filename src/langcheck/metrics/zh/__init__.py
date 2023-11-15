9b8c7d9cd335ee913b96098adaf540b2e468816c
vela <zhongzhen@outlook.jp>
2023-11-16 01:42:53 +0900

    add chinese tokenier and unit test

--- src/langcheck/metrics/zh/__init__.py
+++ src/langcheck/metrics/zh/__init__.py
@@ -1,20 +1,3 @@
-from langcheck.metrics.ja._tokenizers import JanomeTokenizer, MeCabTokenizer
-from langcheck.metrics.ja.reference_based_text_quality import (
-    rouge1, rouge2, rougeL, semantic_similarity)
-from langcheck.metrics.ja.reference_free_text_quality import (
-    fluency, sentiment, tateishi_ono_yamada_reading_ease, toxicity)
-from langcheck.metrics.ja.source_based_text_quality import factual_consistency
+from langcheck.metrics.zh._tokenizers import HanLPTokenizer
 
-__all__ = [
-    'factual_consistency', 
-    'JanomeTokenizer', 
-    'MeCabTokenizer', 
-    'rouge1',
-    'rouge2', 
-    'rougeL', 
-    'semantic_similarity', 
-    'fluency', 
-    'sentiment',
-    'tateishi_ono_yamada_reading_ease', 
-    'toxicity'
-]
+__all__ = ['HanLPTokenizer']
--- src/langcheck/metrics/zh/_tokenizers.py
+++ src/langcheck/metrics/zh/_tokenizers.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 
-import abcip3 install
+import abc
 from collections.abc import Iterator
 from rouge_score.tokenizers import Tokenizer as BaseTokenizer
+
+try:
+    import hanlp
+    # size 43M+, fine grained tokenizer 
+    DEFAULT_TOKENIZER_WEIGHT = hanlp.pretrained.tok.FINE_ELECTRA_SMALL_ZH
+except ModuleNotFoundError:
+    raise ModuleNotFoundError(
+        "No module named 'HanLP'.\n"
+        "Install it by pip install -U hanlp")
 
 # Chinese
 # https://github.com/yikeke/zh-style-guide/blob/master/source/%E6%A0%87%E7%82%B9%E7%AC%A6%E5%8F%B7/%E5%B8%B8%E7%94%A8%E4%B8%AD%E6%96%87%E6%A0%87%E7%82%B9%E7%AC%A6%E5%8F%B7.md
@@ -28,22 +37,29 @@ class _ChineseTokenizer(BaseTokenizer):
             token for token in tokens if (token and token not in _PUNCTUATIONS)
         ]
 
-class PKUTokenizer(_ChineseTokenizer):
-    '''
-    An optional tokenizer to calculate rouge score base PKUTokenizer. 
+
+class HanLPTokenizer(_ChineseTokenizer):
+    '''HanLP based Tokenizer for Chinese.
+
+    The default tokenizer to calculate rouge score base on HanLP.
 
     .. note::
-        website:https://github.com/lancopku/pkuseg-python. Without maintain for
-        over 4 years.
+        HanLP https://github.com/hankcs/HanLP/tree/doc-zh
+        HanLP is the newest tokenizer that have developer maintaince.
+        1.HanLP provide MultiTask Model and Single Task Model, As this module is only
+        used for tokenize, use Single Task Model in default. Multitask Model need download
+        400MB +  pretrained weight, in contrast, Single task only need 40MB + 
+        2.HanLP provide pipeline for concurrency, as LLM generated content always multiple
+        sentence, use pipleline mode.
     '''
-    def __init__(self):
-        try:
-            import pkuseg
-        except ModuleNotFoundError:
-            raise ModuleNotFoundError(
-                "No module named 'jieba'.\n"
-                )
-        self.tokenizer = pkuseg(postag=True)
-    
-    def __tokenize(self,text):
-        return self.tokenizer.cut(text)
+    def __init__(self) -> None:
+        super().__init__()
+        tokenizer = hanlp.load(DEFAULT_TOKENIZER_WEIGHT)
+        self.tokenzier_pipeline = hanlp.pipeline()\
+                                    .append(hanlp.utils.rules.split_sentence) \
+                                    .append(tokenizer)\
+                                    .append(lambda sents: sum(sents, []))
+       
+    def _tokenize(self, text: str) -> Iterator[str]:
+        tokens = self.tokenzier_pipeline(text)
+        return tokens
