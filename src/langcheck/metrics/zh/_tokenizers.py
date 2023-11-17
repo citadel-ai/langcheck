54d3b7b82574cd92ca99423f8b3154e52d40b0a9
vela <zhongzhen@outlook.jp>
2023-11-17 22:33:34 +0900

    flake8 fix

--- src/langcheck/metrics/zh/_tokenizers.py
+++ src/langcheck/metrics/zh/_tokenizers.py
@@ -43,19 +43,19 @@ class HanLPTokenizer(_ChineseTokenizer):
         HanLP https://github.com/hankcs/HanLP/tree/doc-zh
         HanLP is the newest tokenizer that have developer maintaince.
         1.HanLP have multi-task mode and single task mode. Multitask
-        Model need download 400MB+  pretrained weight, in contrast, 
-        single task only need 40MB+. Use single task mode in default. 
+        Model need download 400MB+  pretrained weight, in contrast,
+        single task only need 40MB+. Use single task mode in default.
         2.LLM generated content have lot of sentences in most situtation.
-        use HanLP pipeline mode for concurrency. 
+        use HanLP pipeline mode for concurrency.
     '''
 
     def __init__(self) -> None:
         super().__init__()
         tokenizer = hanlp.load(DEFAULT_TOKENIZER_WEIGHT)
-        self.tokenzier_pipeline = hanlp.pipeline()\
-                                    .append(hanlp.utils.rules.split_sentence) \
-                                    .append(tokenizer)\
-                                    .append(lambda sents: sum(sents, []))
+        self.tokenzier_pipeline = hanlp.pipeline().\
+            append(hanlp.utils.rules.split_sentence).\
+            append(tokenizer).\
+            append(lambda sents: sum(sents, []))
 
     def _tokenize(self, text: str) -> Iterator[str]:
         tokens = self.tokenzier_pipeline(text)
