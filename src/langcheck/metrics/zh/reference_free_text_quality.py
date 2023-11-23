98e5b0288196310d6b7f2810027f0c2424b6d756
vela <zhongzhen@outlook.jp>
2023-11-23 18:21:16 +0900

    add toxicity eval

--- src/langcheck/metrics/zh/reference_free_text_quality.py
+++ src/langcheck/metrics/zh/reference_free_text_quality.py
@@ -16,13 +16,8 @@ from langcheck.metrics.en.reference_free_text_quality import \
 from langcheck.metrics.metric_value import MetricValue
 
 _sentiment_model_path = 'IDEA-CCNL/Erlangshen-Roberta-110M-Sentiment'  # NOQA: E501
-_sentiment_tokenizer = None
-_sentiment_model = None
 
-_toxicity_model_path = "Alnusjaponica/toxicity-score-multi-classification"
-_toxicity_tokenizer_path = "line-corporation/line-distilbert-base-japanese"
-_toxicity_tokenizer = None
-_toxicity_model = None
+_toxicity_model_path = "alibaba-pai/pai-bert-base-zh-llm-risk-detection"
 
 _fluency_model_path = "liwii/fluency-score-classification-ja"
 _fluency_tokenizer_path = "line-corporation/line-distilbert-base-japanese"
@@ -81,7 +76,7 @@ def sentiment(
         metric_value.language = 'ja'
         return metric_value
 
-    global _sentiment_model_path, _sentiment_tokenizer, _sentiment_model
+    global _sentiment_model_path
 
     _sentiment_pipeline = pipeline('sentiment-analysis', model=_sentiment_model_path)  # type: ignore[reportGeneralTypeIssues]  # NOQA: E501
     # {0:"Negative", 1:'Positive'}
@@ -102,3 +97,97 @@ def sentiment(
                        explanations=None,
                        metric_values=scores,  # type: ignore[reportGeneralTypeIssues]  # NOQA: E501
                        language='zh')
+
+
+def toxicity(
+    generated_outputs: List[str] | str,
+    prompts: Optional[List[str] | str] = None,
+    model_type: str = 'local',
+    openai_args: Optional[Dict[str,
+                               str]] = None) -> MetricValue[Optional[float]]:
+    '''Calculates the toxicity scores of generated outputs. This metric takes on
+    float values between [0, 1], where 0 is low toxicity and 1 is high toxicity.
+    (NOTE: when using the OpenAI model, the toxicity scores are in steps of
+    0.25. The score may also be `None` if it could not be computed.)
+
+    We currently support two model types:
+    1. The 'local' type, where a model file is downloaded from HuggingFace and
+    run locally. This is the default model type and there is no setup needed to
+    run this.
+    The model (Alnusjaponica/toxicity-score-multi-classification) is a
+    fine-tuned model based on line-corporation/line-distilbert-base-japanese
+    model.
+    2. The 'openai' type, where we use OpenAI's 'gpt-turbo-3.5' model
+    by default, in the same way as english counterpart. While the model you use
+    is configurable, please make sure to use one that supports function calling
+    (https://platform.openai.com/docs/guides/gpt/function-calling). See
+    `this example <https://langcheck.readthedocs.io/en/latest/metrics.html
+    #computing-metrics-with-openai-models>`__
+    for examples on setting up the OpenAI API key.
+
+    Ref:
+        "alibaba-pai/pai-bert-base-zh-llm-risk-detection"
+
+    Args:
+        generated_outputs: The model generated output(s) to evaluate
+        prompts: The prompts used to generate the output(s). Prompts are
+            optional metadata and not used to calculate the metric.
+        model_type: The type of model to use ('local' or 'openai'),
+            default 'local'
+        openai_args: Dict of additional args to pass in to the
+            `openai.ChatCompletion.create` function, default None
+
+    Returns:
+        An :class:`~langcheck.metrics.metric_value.MetricValue` object
+    '''
+    generated_outputs, prompts = validate_parameters_reference_free(
+        generated_outputs, prompts)
+    assert model_type in ['local', 'openai'
+                         ], ('Unsupported model type. '
+                             'The supported ones are ["local", "openai"]')
+
+    if model_type == 'local':
+        scores = _toxicity_local(generated_outputs)
+        explanations = None
+    else:  # openai
+        scores, explanations = _toxicity_openai(generated_outputs, openai_args)
+
+    return MetricValue(metric_name='toxicity',
+                       prompts=prompts,
+                       generated_outputs=generated_outputs,
+                       reference_outputs=None,
+                       sources=None,
+                       explanations=explanations,
+                       metric_values=scores,
+                       language='zh')
+
+
+def _toxicity_local(generated_outputs: List[str]) -> List[float]:
+    '''Calculates the toxicity scores of generated outputs using a fine-tuned
+    model from `alibaba-pai/pai-bert-base-zh-llm-risk-detection`. This metric
+    takes on float values between [0, 1], where 0 is low toxicity and 1 is high
+    toxicity.
+
+    Ref:
+        https://huggingface.co/alibaba-pai/pai-bert-base-zh-llm-risk-detection
+
+    Args:
+        generated_outputs: A list of model generated outputs to evaluate
+
+    Returns:
+        A list of scores
+    '''
+    global _toxicity_model_path
+    # this pipeline output predict probability for each text on each label.
+    # the output format is List[List[Dict(str)]]
+    _toxicity_pipeline = pipeline('text-classification', model=_toxicity_model_path, top_k=5)  # type: ignore[reportGeneralTypeIssues]  # NOQA: E501
+    # {'Normal': 0, 'Pulp': 1, 'Sex': 2, 'Other Risk': 3, 'Adult': 4}
+    _model_id2label = _toxicity_pipeline.model.config.id2label
+    _predict_results = _toxicity_pipeline(generated_outputs)  # type: ignore[reportGeneralTypeIssues]  # NOQA: E501
+
+    toxicity_scores = [
+        [1 - _score['score'] for _score in item_predict_proba if _score['label'] == _model_id2label[0]]  # type: ignore[reportGeneralTypeIssues]  # NOQA: E501
+        for item_predict_proba in _predict_results]
+    # flatten list
+    toxicity_scores = sum(toxicity_scores, [])
+    return toxicity_scores  # type: ignore[reportGeneralTypeIssues]
