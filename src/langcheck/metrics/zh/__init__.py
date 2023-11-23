d3fbeba210691999bddcc14b2f0943132a411e96
vela <zhongzhen@outlook.jp>
2023-11-23 19:26:41 +0900

    add toxicity test case

--- src/langcheck/metrics/zh/__init__.py
+++ src/langcheck/metrics/zh/__init__.py
@@ -2,9 +2,10 @@ from langcheck.metrics.zh._tokenizers import HanLPTokenizer
 from langcheck.metrics.zh.reference_based_text_quality import (
     rouge1, rouge2, rougeL, semantic_similarity)
 from langcheck.metrics.zh.source_based_text_quality import factual_consistency
-from langcheck.metrics.zh.reference_free_text_quality import (sentiment)
+from langcheck.metrics.zh.reference_free_text_quality import (sentiment,
+                                                              toxicity)
 __all__ = [
     'HanLPTokenizer', 'semantic_similarity', 'rouge1', 'rouge2', 'rougeL',
     'factual_consistency',
-    'sentiment'
+    'sentiment', 'toxicity'
 ]
--- tests/metrics/zh/test_reference_free_text_quality.py
+++ tests/metrics/zh/test_reference_free_text_quality.py
@@ -2,7 +2,7 @@ from unittest.mock import Mock, patch
 
 import pytest
 
-from langcheck.metrics.zh import (sentiment)
+from langcheck.metrics.zh import (sentiment, toxicity)
 from tests.utils import is_close
 
 ################################################################################
@@ -42,4 +42,37 @@ def test_sentiment_openai(generated_outputs):
                Mock(return_value=mock_chat_response)):
         metric_value = sentiment(generated_outputs, model_type='openai')
         # "Positive" gets a value of 1.0
+        assert metric_value == 1
+
+
+@pytest.mark.parametrize('generated_outputs',
+                         ['我今天生病了。', ['我今天生病了。'], ['我今天生病了。', '你有病啊。']])
+def test_toxicity(generated_outputs):
+    metric_value = toxicity(generated_outputs)
+    assert 0 <= metric_value <= 1
+    if len(metric_value.metric_values) == 2:
+        toxicity_score_high_risk = metric_value.metric_values[1]
+        assert toxicity_score_high_risk is not None
+        assert 0.5 <= toxicity_score_high_risk <= 1
+
+
+@pytest.mark.parametrize('generated_outputs',
+                         ['我今天生病了。', ['我今天生病了。'], ['我今天生病了。', '你有病啊。']])
+def test_toxicity_openai(generated_outputs):
+    mock_chat_response = {
+        'choices': [{
+            'message': {
+                'function_call': {
+                    'arguments': "{\n  \"toxicity\": \"5\"\n}"
+                },
+                'content': 'foo bar'
+            }
+        }]
+    }
+    # Calling the openai.ChatCompletion.create method requires an OpenAI API
+    # key, so we mock the return value instead
+    with patch('openai.ChatCompletion.create',
+               Mock(return_value=mock_chat_response)):
+        metric_value = toxicity(generated_outputs, model_type='openai')
+        # "5" gets a value of 1.0
         assert metric_value == 1
