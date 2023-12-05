bfc89f76d933636505a4b4d4beee7dfb2313d96a
Vela-zz <59645193+Vela-zz@users.noreply.github.com>
2023-12-05 23:54:49 +0900

    Apply suggestions from code review
    Co-authored-by: Yosuke Higashi <107823399+yosukehigashi@users.noreply.github.com>

--- src/langcheck/metrics/zh/reference_free_text_quality.py
+++ src/langcheck/metrics/zh/reference_free_text_quality.py
@@ -232,13 +232,13 @@ def _toxicity_local(generated_outputs: List[str]) -> List[float]:
 
 def xuyaochen_report_readability(
         generated_outputs: List[str]) -> MetricValue[float]:
-    '''Calculates the readibility scores of generated outputs introduced in
-    "中文年报可读性"(Chinese annual report readability). This metrics calcuate
-    average words per sentence as r1, average adverb numbres and connection
+    '''Calculates the readability scores of generated outputs introduced in
+    "中文年报可读性"(Chinese annual report readability). This metric calculates
+    average words per sentence as r1, average adverb numbers and connection
     words as r2, then, refer to the Fog Indexto combine the r1 and r2.
-    This function use HanLP Tokenizer and Pos at the same time, POS
-    in CTB style https://hanlp.hankcs.com/docs/annotations/pos/ctb.html。
-    This lower the score is, the better the readability. the score is mainly
+    This function uses HanLP Tokenizer and POS at the same time, POS
+    in CTB style https://hanlp.hankcs.com/docs/annotations/pos/ctb.html.
+    The lower the score is, the better the readability. The score is mainly
     influenced by r1, the average words in readabilities.
 
     Ref:
