7987b0b4370be525eddce391da81521d9e16bdac
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-12-19 15:55:12 +0900

    add "ja" to langcheck.augment

--- src/langcheck/augment/__init__.py
+++ src/langcheck/augment/__init__.py
@@ -1,8 +1,15 @@
-from langcheck.augment import en
-from langcheck.augment.en import (change_case, gender, keyboard_typo, ocr_typo,
-                                  remove_punctuation, rephrase, synonym)
+from langcheck.augment import en, ja
+from langcheck.augment.en import (
+    change_case,
+    gender,
+    keyboard_typo,
+    ocr_typo,
+    remove_punctuation,
+    rephrase,
+    synonym,
+)
 
 __all__ = [
-    "change_case", "en", "gender", "keyboard_typo", "ocr_typo", "synonym",
+    "change_case", "en", "gender", "ja", "keyboard_typo", "ocr_typo", "synonym",
     "remove_punctuation", "rephrase"
 ]
