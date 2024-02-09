ff9038150bdeff52cb7400541a299fc6ea7bd37e
Yosuke Higashi <yosuke.higashi1@gmail.com>
2024-02-09 01:52:16 +0000

    clean load_auto_model_for_seq2seq docstring

--- src/langcheck/metrics/model_manager/_model_loader.py
+++ src/langcheck/metrics/model_manager/_model_loader.py
@@ -66,18 +66,23 @@ def load_auto_model_for_seq2seq(
     model_name: str,
     tokenizer_name: Optional[str] = None,
     revision: Optional[str] = None
-) -> Tuple[AutoTokenizer, AutoModelForSequenceClassification]:
+) -> Tuple[AutoTokenizer, AutoModelForSeq2SeqLM]:
     '''
-    Return a sequence to sequence model availble on huggingface hub.
+    Loads a sequence-to-sequence model and its tokenizer.
 
     Args:
-        model_name: The name of a sequence-classification model on Hugging Face
-        tokenizer_name: The name of a tokenizer on Hugging Face
-        revision: The shortened sha1 string of a model
+        model_name: The name of the sequence-classification model to load.
+        tokenizer_name: The name of the tokenizer to load. If None, the
+            tokenizer associated with the model will be loaded.
+        revision: The model revision to load.
+
+    Returns:
+        tokenizer: The loaded tokenizer.
+        model: The loaded sequence-to-sequence model.
     '''
     if tokenizer_name is None:
         tokenizer_name = model_name
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, revision=revision)
     model = AutoModelForSeq2SeqLM.from_pretrained(
         model_name, revision=revision)  # NOQA: E501
-    return tokenizer, model
+    return tokenizer, model  # type: ignore
