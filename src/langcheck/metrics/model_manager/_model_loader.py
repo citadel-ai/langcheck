c46645bedd91b840b9e2d28a062ae6ae32216a5a
Yosuke Higashi <yosuke.higashi1@gmail.com>
2024-02-09 01:49:01 +0000

    clean load_auto_model_for_text_classification docstring

--- src/langcheck/metrics/model_manager/_model_loader.py
+++ src/langcheck/metrics/model_manager/_model_loader.py
@@ -42,19 +42,24 @@ def load_auto_model_for_text_classification(
     revision: Optional[str] = None
 ) -> Tuple[AutoTokenizer, AutoModelForSequenceClassification]:
     '''
-    Return a sequence classification model on huggingface hub.
+    Loads a sequence classification model and its tokenizer.
 
     Args:
-        model_name: The name of a sequence-classification model on Hugging Face
-        tokenizer_name: The name of a tokenizer on Hugging Face
-        revision: The shortened sha1 string of a model
+        model_name: The name of the sequence-classification model to load.
+        tokenizer_name: The name of the tokenizer to load. If None, the
+            tokenizer associated with the model will be loaded.
+        revision: The model revision to load.
+
+    Returns:
+        tokenizer: The loaded tokenizer.
+        model: The loaded sequence classification model.
     '''
     if tokenizer_name is None:
         tokenizer_name = model_name
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, revision=revision)
     model = AutoModelForSequenceClassification.from_pretrained(
-        model_name, revision=revision)  # NOQA: E501
-    return tokenizer, model
+        model_name, revision=revision)
+    return tokenizer, model  # type: ignore
 
 
 def load_auto_model_for_seq2seq(
