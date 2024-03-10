9a50b1242658d39d91591d823a9eb802b00fa38c
Kenny Song <kenny.ysong@gmail.com>
2024-03-10 07:18:33 +0000

    Use lazy import

--- src/langcheck/metrics/__init__.py
+++ src/langcheck/metrics/__init__.py
@@ -1,3 +1,6 @@
+import importlib.util
+import sys
+
 from langcheck.metrics.en.reference_based_text_quality import (
     rouge1, rouge2, rougeL, semantic_similarity)
 from langcheck.metrics.en.reference_free_text_quality import (
@@ -13,7 +16,38 @@ from langcheck.metrics.text_structure import (contains_all_strings,
                                               is_json_array, is_json_object,
                                               matches_regex, validation_fn)
 
+
+def lazy_import(name):
+    '''Lazily import the language-specific packages in langcheck.metrics.
+
+    This prevents `import langcheck` from throwing ModuleNotFoundError if the 
+    user hasn't installed `langcheck[ja]`, while still allowing the package
+    `langcheck.metrics.ja` to be visible even if the user didn't explicitly run
+    `import langcheck.metrics.ja`.
+
+    Copied from: https://docs.python.org/3/library/importlib.html#implementing-lazy-imports
+    '''
+    spec = importlib.util.find_spec(name)
+    assert spec is not None and spec.loader is not None  # For type checking
+    loader = importlib.util.LazyLoader(spec.loader)
+    spec.loader = loader
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    loader.exec_module(module)
+    return module
+
+
+# Use lazy import instead of directly importing language-specific packages
+en = lazy_import('langcheck.metrics.en')
+ja = lazy_import('langcheck.metrics.ja')
+de = lazy_import('langcheck.metrics.de')
+zh = lazy_import('langcheck.metrics.zh')
+
 __all__ = [
+    'en',
+    'ja',
+    'de',
+    'zh',
     'ai_disclaimer_similarity',
     'answer_relevance',
     'contains_all_strings',
