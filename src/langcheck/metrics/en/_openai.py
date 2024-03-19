fb602133c612a210737bde0bed7861a9014a2057
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2024-03-19 11:09:44 +0900

    Change input of get_score() from str to iterator or list

--- src/langcheck/metrics/en/_openai.py
+++ src/langcheck/metrics/en/_openai.py
@@ -1,8 +1,11 @@
 import json
 import os
-from typing import Callable, Dict, Optional, Tuple
+from typing import (Any, Callable, Dict, Iterator, List, Optional, Sequence,
+                    Tuple)
 
 from openai import AzureOpenAI, OpenAI
+
+from langcheck.utils.progess_bar import tqdm_wrapper
 
 
 class OpenAIBasedEvaluator:
@@ -61,8 +64,10 @@ class OpenAIBasedEvaluator:
         self._openai_args = openai_args
 
     def get_score(
-        self, prompt: str, function_call_prompt_template: Callable
-    ) -> Tuple[Optional[float], Optional[str]]:
+        self,
+        prompts: Iterator[str] | Sequence[str],
+        function_call_prompt_template: Callable,
+    ) -> Tuple[List[float | None], List[str | None]]:
         '''
         Retrieves the score and unstructured assessment for a given prompt using
         the OpenAI API. The first API call is a "normal" call, where the API
@@ -73,46 +78,47 @@ class OpenAIBasedEvaluator:
         assessment.
 
         Args:
-            prompt: Prompt that asks the OpenAI API for the unstructured
-                assessment response
+            prompts: :List of prompt that asks the OpenAI API for the
+                unstructured assessment response
             function_call_prompt_template: Prompt template used to construct the
                 prompt that asks the OpenAI API for the structured assessment
                 response
 
         Returns:
-            score: score associated with the given prompt based on the resulting
-                structured assessment. Returns `None` if the score could not be
-                computed.
-            unstructured_assessment: unstructured assessment text, which also
-                serves as the explanation of the score. Returns `None` if the
-                score could not be computed.
+            scores: List of scores associated with the corresponding prompts
+                based on the resulting structured assessment. If a score cannot
+                be computed for a prompt, it will be represented as `None`.
+            unstructured_assessments: List of unstructured assessment text,
+                which also serves as the explanation of the score. If a score
+                cannot be computed for a prompt, it will be represented as
+                `None`.
         '''
         # First, call the API to get an unstructured assessment. The response
         # should include both the assessment itself and the model's reasoning
         # for that assessment.
-        messages = [{"role": "user", "content": prompt}]
-        try:
-            if self._openai_args is None:
-                response = self._client.chat.completions.create(
-                    model="gpt-3.5-turbo", seed=123,
-                    messages=messages)  # type: ignore
-            else:
-                response = self._client.chat.completions.create(
-                    messages=messages,  # type: ignore
-                    seed=123,
-                    **self._openai_args)  # type: ignore
-            unstructured_assessment = response.choices[0].message.content
-        except Exception as e:
-            print(f'OpenAI failed to return an unstructured assessment: {e}')
-            print(f'Prompt that triggered the failure is:\n{prompt}')
-            return None, None
+        # Call the API to get an unstructured assessments.
+        config_unstructured_assessments = {
+            "model": "gpt-3.5-turbo",
+            "seed": 123
+        }
+        config_unstructured_assessments.update(self._openai_args or {})
+
+        responses = self._call_api(prompts=prompts,
+                                   config=config_unstructured_assessments)
+        unstructured_assessments = [
+            response.choices[0].message.content if response else None
+            for response in responses
+        ]
 
         # Next, call the API leveraging function calling to get a structured
-        # assessment
-        fn_call_messages = [{
-            "role": "user",
-            "content": function_call_prompt_template(unstructured_assessment)
-        }]
+        # assessment.
+        # Construct the prompt for the function calling API, filled with None
+        # for the failed unstructured assessment.
+        fn_call_messages = [
+            function_call_prompt_template(unstructured_assessment)
+            if unstructured_assessment else None
+            for unstructured_assessment in unstructured_assessments
+        ]
         argument_options = list(self._assessment_to_score_mapping.keys())
         functions = [{
             "name": self._function_name,
@@ -129,38 +135,72 @@ class OpenAIBasedEvaluator:
                 "required": [self._argument_name],
             },
         }]
-        try:
-            if self._openai_args is None:
-                response = self._client.chat.completions.create(
-                    model="gpt-3.5-turbo",
-                    messages=fn_call_messages,  # type: ignore
-                    seed=123,
-                    functions=functions,  # type: ignore
-                    function_call={"name": self._function_name})
-            else:
-                response = self._client.chat.completions.create(  # type: ignore
-                    messages=fn_call_messages,  # type: ignore
-                    seed=123,
-                    functions=functions,  # type: ignore
-                    function_call={"name": self._function_name},
-                    **self._openai_args)  # type: ignore
-            # For type checking
-            assert response.choices[0].message.function_call is not None
-            function_args = json.loads(
-                response.choices[0].message.function_call.arguments)
-            assessment = function_args.get(self._argument_name)
-        except Exception as e:
-            print(f'OpenAI failed to return a structured assessment: {e}')
-            print('Prompt that triggered the failure is:\n'
-                  f'{function_call_prompt_template(unstructured_assessment)}')
-            return None, None
+        config_structured_assessments = {
+            "seed": 123,
+            "functions": functions,
+            "function_call": {
+                "name": self._function_name
+            },
+            "model": "gpt-3.5-turbo"
+        }
+        config_structured_assessments.update(self._openai_args or {})
 
-        if assessment not in self._assessment_to_score_mapping:
+        responses = self._call_api(
+            prompts=fn_call_messages,
+            config=config_structured_assessments,
+        )
+        function_args = [
+            json.loads(response.choices[0].message.function_call.arguments)
+            if response else None for response in responses
+        ]
+        assessments = [
+            function_arg.get(self._argument_name) if function_arg else None
+            for function_arg in function_args
+        ]
+
+        # Check if any of the assessments are not recognized.
+        for assessment in assessments:
+            if (assessment is None) or (assessment
+                                        in self._assessment_to_score_mapping):
+                continue
             # By leveraging the function calling API, this should be pretty
-            # rare, but we're dealing with LLMs here so nothing is absolute!
+            # rare, but we're dealing with LLMs here so nothing is
+            # absolute!
             print(f'OpenAI returned an unrecognized assessment: "{assessment}"')
-            print(f'Prompt that triggered the failure is:\n{prompt}')
-            return None, None
 
-        return self._assessment_to_score_mapping[
-            assessment], unstructured_assessment
+        return [
+            self._assessment_to_score_mapping[assessment]
+            if assessment else None for assessment in assessments
+        ], unstructured_assessments
+
+    def _call_api(self, prompts: Iterator[str | None] | Sequence[str | None],
+                  config: Dict[str, str]) -> List[Any]:
+        # Generates input dict for API call. This procedure is separated as a
+        # method because yapf fails when there are too much nests.
+        def _generate_model_input(prompt: str | None) -> Dict[str, Any]:
+            return {"messages": [{"role": "user", "content": prompt}], **config}
+
+        # A helper function to call the API with exception filter for alignment
+        # of exception handling with the async version.
+        def _call_api_with_exception_filter(model_input: Dict[str, Any]) -> Any:
+            if model_input is None:
+                return None
+            try:
+                return self._client.chat.completions.create(**model_input)
+            except Exception as e:
+                return e
+
+        model_inputs = map(_generate_model_input, prompts)
+        responses = [
+            _call_api_with_exception_filter(model_input)
+            for model_input in tqdm_wrapper(model_inputs)
+        ]
+
+        # Filter out exceptions and print them out.
+        for i, response in enumerate(responses):
+            if not isinstance(response, Exception):
+                continue
+            print('OpenAI failed to return an assessment corresponding to '
+                  f'{i}th prompt: {response}')
+            responses[i] = None
+        return responses
