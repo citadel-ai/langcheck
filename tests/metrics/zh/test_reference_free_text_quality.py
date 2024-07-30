41e6fa76e4543d466ba0eb861640e292ae3dc878
Hao Wang <50416856+conan1024hao@users.noreply.github.com>
2024-07-30 13:18:03 +0900

    Update test_reference_free_text_quality.py

--- tests/metrics/zh/test_reference_free_text_quality.py
+++ tests/metrics/zh/test_reference_free_text_quality.py
@@ -35,7 +35,8 @@ def test_sentiment(generated_outputs):
 def test_sentiment_eval_client(generated_outputs):
     eval_client = MockEvalClient()
     metric_value = sentiment(generated_outputs, eval_model=eval_client)
-    assert metric_value.metric_values[0] is None  # MockEvalClient without any argument returns None
+    # MockEvalClient without any argument returns None
+    assert metric_value.metric_values[0] is None
     sentiment_assessment_to_score = {
         "Positive": 1.0,
         "Neutral": 0.5,
@@ -54,8 +55,7 @@ def test_sentiment_eval_client(generated_outputs):
 def test_toxicity(generated_outputs):
     metric_value = toxicity(generated_outputs)
     toxicity_score_low_risk = metric_value.metric_values[0]
-    # type: ignore[reportGeneralTypeIssues]
-    assert 0 <= toxicity_score_low_risk <= 0.6
+    assert 0 <= toxicity_score_low_risk <= 0.6  # type: ignore[reportGeneralTypeIssues]
     if len(metric_value.metric_values) == 2:
         toxicity_score_high_risk = metric_value.metric_values[1]
         assert toxicity_score_high_risk is not None
