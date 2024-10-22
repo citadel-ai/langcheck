3676935f7ad1ff77a87804c8477b4a091e678afd
Yoshiki Tanioka <taniokayoshiki@gmail.com>
2024-10-22 05:36:49 +0000

    format [no ci]

--- tests/augment/en/test_rephrase.py
+++ tests/augment/en/test_rephrase.py
@@ -7,43 +7,67 @@ import pytest
 from openai.types.chat import ChatCompletion
 
 from langcheck.augment.en import rephrase
-from langcheck.metrics.eval_clients import OpenAIEvalClient, AzureOpenAIEvalClient
+from langcheck.metrics.eval_clients import (
+    AzureOpenAIEvalClient,
+    OpenAIEvalClient,
+)
 
 
 @pytest.mark.parametrize(
     "instances, num_perturbations, expected",
     [
-        ("List three representative testing methods for LLMs.", 1,
-         ["Identify three typical methods used for evaluating LLMs."]),
-        (["List three representative testing methods for LLMs."], 2, [
-            "Identify three typical methods used for evaluating LLMs.",
-            "Identify three typical methods used for evaluating LLMs."
-        ]),
+        (
+            "List three representative testing methods for LLMs.",
+            1,
+            ["Identify three typical methods used for evaluating LLMs."],
+        ),
+        (
+            ["List three representative testing methods for LLMs."],
+            2,
+            [
+                "Identify three typical methods used for evaluating LLMs.",
+                "Identify three typical methods used for evaluating LLMs.",
+            ],
+        ),
     ],
 )
-def test_rephrase(instances: list[str] | str, num_perturbations: int,
-                  expected: list[str]):
+def test_rephrase(
+    instances: list[str] | str, num_perturbations: int, expected: list[str]
+):
     mock_chat_completion = Mock(spec=ChatCompletion)
     mock_chat_completion.choices = [
-        Mock(message=Mock(
-            content="Identify three typical methods used for evaluating LLMs."))
+        Mock(
+            message=Mock(
+                content="Identify three typical methods used for evaluating LLMs."
+            )
+        )
     ]
     # Calling the openai.ChatCompletion.create method requires an OpenAI API
     # key, so we mock the return value instead
-    with patch("openai.resources.chat.Completions.create",
-               return_value=mock_chat_completion):
+    with patch(
+        "openai.resources.chat.Completions.create",
+        return_value=mock_chat_completion,
+    ):
         # Set the necessary env vars for the 'openai' model type
         os.environ["OPENAI_API_KEY"] = "dummy_key"
         openai_client = OpenAIEvalClient()
-        actual = rephrase(instances, num_perturbations=num_perturbations, eval_client=openai_client)
+        actual = rephrase(
+            instances,
+            num_perturbations=num_perturbations,
+            eval_client=openai_client,
+        )
         assert actual == expected
 
         # Set the necessary env vars for the 'azure_openai' model type
         os.environ["AZURE_OPENAI_KEY"] = "dummy_azure_key"
         os.environ["OPENAI_API_VERSION"] = "dummy_version"
         os.environ["AZURE_OPENAI_ENDPOINT"] = "dummy_endpoint"
-        azure_openai_client = AzureOpenAIEvalClient(embedding_model_name="foo bar")
-        actual = rephrase(instances,
-                          num_perturbations=num_perturbations,
-                          eval_client=azure_openai_client)
+        azure_openai_client = AzureOpenAIEvalClient(
+            embedding_model_name="foo bar"
+        )
+        actual = rephrase(
+            instances,
+            num_perturbations=num_perturbations,
+            eval_client=azure_openai_client,
+        )
         assert actual == expected
