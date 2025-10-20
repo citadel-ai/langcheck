6915fe7a0b029fcff5d7fc52f85acaf557ad93dd
emma <emma@citadel-ai.com>
2025-10-20 13:09:24 +0900

    Revert "remove anthropic client"
    This reverts commit 60669ff0735e44944217451f265130f1688d1a06.

--- src/langcheck/metrics/eval_clients/__init__.py
+++ src/langcheck/metrics/eval_clients/__init__.py
@@ -33,6 +33,26 @@ __all__ = [
 ]
 
 try:
+    from langcheck.metrics.eval_clients._anthropic import (
+        AnthropicEvalClient,  # NOQA: F401
+        AnthropicExtractor,  # NOQA: F401
+    )
+except ModuleNotFoundError:
+    pass
+else:
+    __all__.extend(["AnthropicEvalClient", "AnthropicExtractor"])
+
+try:
+    from langcheck.metrics.eval_clients._gemini import (
+        GeminiEvalClient,  # NOQA: F401
+        GeminiExtractor,  # NOQA: F401
+    )
+except ModuleNotFoundError:
+    pass
+else:
+    __all__.extend(["GeminiEvalClient", "GeminiExtractor"])
+
+try:
     from langcheck.metrics.eval_clients._llama import (
         LlamaEvalClient,  # NOQA: F401
         LlamaExtractor,  # NOQA: F401
